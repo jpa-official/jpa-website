99d571e37442ab70988617af41ea6a4589efe286
이원주 <iwonju@wonjuui-somaegbug.local>
2026-08-21 17:37:27 +0900

    Update OZX thumbnail and insert main logo page before symbol+wordmark
    - Swap the thumbnail for the provided logotype+symbol+wordmark lockup
      (with the SPACE CONTENT OPERATOR tagline) on white
    - Insert the main logo construction page (14) into the gallery right
      before the symbol+wordmark lockup for a clearer logo-building story

--- js/projects-data.js
+++ js/projects-data.js
@@ -112,7 +112,8 @@ window.PROJECTS = [
       'images/projects/ozx-branding-5.jpg',
       'images/projects/ozx-branding-6.jpg',
       'images/projects/ozx-branding-7.jpg',
-      'images/projects/ozx-branding-8.jpg'
+      'images/projects/ozx-branding-8.jpg',
+      'images/projects/ozx-branding-9.jpg'
     ],
     name: 'OZX',
     nameKo: '오즈엑스',
